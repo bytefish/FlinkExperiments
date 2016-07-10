d62c9a049f14d8dc3756cf12e5ac7f970beec818
Philipp Wagner <bytefish@gmx.de>
2016-07-10 12:38:02 +0200

    Process LocalTime for now

--- FlinkExperiments/src/main/java/app/WeatherDataStreamingExample.java
+++ FlinkExperiments/src/main/java/app/WeatherDataStreamingExample.java
@@ -43,7 +43,7 @@ public class WeatherDataStreamingExample {
                 .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<LocalWeatherData>() {
                     @Override
                     public long extractAscendingTimestamp(LocalWeatherData localWeatherData) {
-                        Date measurementTime = DateUtilities.from(localWeatherData.getDate(), localWeatherData.getTime(), ZoneOffset.ofHours(localWeatherData.getStation().getTimeZone()));
+                        Date measurementTime = DateUtilities.from(localWeatherData.getDate(), localWeatherData.getTime(), ZoneOffset.ofHours(0));
 
                         return measurementTime.getTime();
                     }
